2de59d33525fd49435f6cba5353a41aadd0e236a
Hugo Tse <hugotse8@gmail.com>
2022-07-16 12:16:55 -0700

    changes for v2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is V2
         </p>
-
       </header>
     </div>
   );
